4ef2fe60bfee36c6e54b3a3f63ee9827ca5418de
Konstantin <drpdrp@mail.ru>
2021-12-29 20:21:22 +0300

    ADD: false + test

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,12 +1,5 @@
 use std::time::{Duration, SystemTime};
 
 fn main() {
-    let now = SystemTime::now();
-
-    // emulate some work
-    std::thread::sleep(Duration::from_millis(1100));
-
-    let elapsed = now.elapsed().unwrap();
-
-    println!("{:?}", elapsed);
+    println!("Hello, world!");
 }
