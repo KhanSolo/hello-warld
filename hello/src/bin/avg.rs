37b176ac9d3d13bdd800bcb83aa97814f232df37
Konstantin <drpdrp@mail.ru>
2021-12-29 20:56:26 +0300

    ADD: avg

---
+++ hello/src/bin/avg.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let arr = [1., 2., 3., 4., 5., 6., 7., 8.];
+    println!("{}", avg(&arr));
+    println!("{}", avg2(&arr));
+
+    std::process::exit(0); 
+}
+
+fn avg(list: &[f64]) -> f64 {
+    let mut total = 0.;
+
+    for el in list {
+        total += *el;
+    }
+    return total / list.len() as f64;
+}
+
+fn avg2(list: &[f64]) -> f64 {
+    list.iter().sum::<f64>() / list.len() as f64
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,3 @@
-use std::time::{Duration, SystemTime};
-
 fn main() {
     println!("Hello, world!");
 }
