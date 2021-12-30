6770bfc1b085a05e33f0adb82d74310db83c6b9a
Konstantin <drpdrp@mail.ru>
2021-12-30 11:44:28 +0300

    CHG: avg: variant with fold()

--- hello/src/bin/avg.rs
+++ hello/src/bin/avg.rs
@@ -4,8 +4,9 @@ fn main() {
     let arr = [1., 2., 3., 4., 5., 6., 7., 8.];
     println!("{}", avg(&arr));
     println!("{}", avg2(&arr));
+    println!("{}", avg4(&arr));
 
-    std::process::exit(0); 
+    std::process::exit(0);
 }
 
 fn avg(list: &[f64]) -> f64 {
@@ -25,3 +26,7 @@ fn avg2(list: &[f64]) -> f64 {
 // fn avg3(list: &[f64]) -> f64 {
 //     list.par_iter().sum::<f64>() / list.len() as f64
 // }
+
+fn avg4(list: &[f64]) -> f64 {
+    list.iter().fold(0., |a, b| a + b) / list.len() as f64
+}
--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,4 +1,4 @@
-use assert_cmd::{Command};
+use assert_cmd::Command;
 
 #[test]
 fn sample() {
@@ -9,19 +9,18 @@ fn sample() {
 
 #[test]
 fn runs() {
-    let mut cmd = Command::cargo_bin("hello").unwrap();    
-    cmd.assert().success().stdout("Hello, world!\n"); 
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
 }
 
-
 #[test]
-fn true_ok(){
+fn true_ok() {
     let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
 
 #[test]
-fn false_not_ok(){
+fn false_not_ok() {
     let mut cmd = Command::cargo_bin("false").unwrap();
     cmd.assert().failure();
 }
