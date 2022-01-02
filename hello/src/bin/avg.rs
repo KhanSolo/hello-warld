2cd3b3a14bf19ab3e6a7ac052c6a6ca88bfdd38b
Konstantin Ta <drpdrp@mail.ru>
2022-01-02 12:19:20 +0300

    FIX: rayon issues

--- hello/src/bin/avg.rs
+++ hello/src/bin/avg.rs
@@ -1,15 +1,16 @@
-//use rayon::prelude::*;
+use rayon::prelude::*;
 
 fn main() {
     let arr = [1., 2., 3., 4., 5., 6., 7., 8.];
-    println!("{}", avg(&arr));
+    println!("{}", avg1(&arr));
     println!("{}", avg2(&arr));
+    println!("{}", avg3(&arr));
     println!("{}", avg4(&arr));
 
     std::process::exit(0);
 }
 
-fn avg(list: &[f64]) -> f64 {
+fn avg1(list: &[f64]) -> f64 {
     let mut total = 0.;
 
     for el in list {
@@ -22,10 +23,9 @@ fn avg2(list: &[f64]) -> f64 {
     list.iter().sum::<f64>() / list.len() as f64
 }
 
-// todo resolve issue with rayon import
-// fn avg3(list: &[f64]) -> f64 {
-//     list.par_iter().sum::<f64>() / list.len() as f64
-// }
+fn avg3(list: &[f64]) -> f64 {
+    list.par_iter().sum::<f64>() / list.len() as f64
+}
 
 fn avg4(list: &[f64]) -> f64 {
     list.iter().fold(0., |a, b| a + b) / list.len() as f64
