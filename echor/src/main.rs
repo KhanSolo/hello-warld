dc85137f624ac8e5c6043cfaf44e29a217303a94
Konstantin Ta <drpdrp@mail.ru>
2022-01-13 00:06:31 +0300

    ADD: echor: first test

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,27 +1,25 @@
 use clap::{App, Arg};
 
 fn main() {
-    
     let matches = App::new("echor")
         .version("0.1.0")
         .author("KonstT")
         .about("rust echo")
         .arg(
             Arg::new("text")
-            .value_name("TEXT")
-            .help("Input text")
-            .required(true)
-            .min_values(1),
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
         )
         .arg(
             Arg::new("omit_newline")
-            .help("Do not print newline")
-            .takes_value(false)
-            .short('n'),
+                .help("Do not print newline")
+                .takes_value(false)
+                .short('n'),
         )
         .get_matches();
 
-    //println!("{:#?}", matches);
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
     print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
