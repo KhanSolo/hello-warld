b3ded8def4ec2d9a5c39d57e13a50947a80651f5
Konstantin Ta <drpdrp@mail.ru>
2022-01-12 23:50:36 +0300

    CHG: echor: adding cmdline arguments

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,4 +1,4 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
     
@@ -6,6 +6,19 @@ fn main() {
         .version("0.1.0")
         .author("KonstT")
         .about("rust echo")
+        .arg(
+            Arg::new("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+            .help("Do not print newline")
+            .takes_value(false)
+            .short('n'),
+        )
         .get_matches();
 
     println!("{:#?}", matches);
